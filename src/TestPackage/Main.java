ecfe4c557bbca87e52b3f3035cd0c80e26ababa5
Thomas <Thomas@Tannenberg.online>
2023-11-08 13:28:06 +0100

    Erster Upload von IntelliJ

--- src/TestPackage/Main.java
+++ src/TestPackage/Main.java
@@ -3,19 +3,19 @@ package TestPackage;
 public class Main {
     public static void main(String[] args) {
         try {
-            // Start the code inspection
-            System.out.println("Suche nach bugs...");
-            Thread.sleep(1000); // Simulate code inspection time
 
-            // If a bug is found
+            System.out.println("Suche nach bugs...");
+            Thread.sleep(1000);
+
+
             if (Math.random() > 0.5) {
                 throw new Exception("BUG GEFUNDEN!");
             }
 
-            // If no bugs were found
+
             System.out.println("KEINE BUG gefunden. Es ist ein Wunder");
         } catch (Exception e) {
-            // Handle the bug
+
             System.out.println(e.getMessage());
             System.out.println("Lösche Bug...");
             // Bug squashed
