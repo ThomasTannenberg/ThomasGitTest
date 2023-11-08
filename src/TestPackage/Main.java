b5367298fbfff6a976f56dd0e5fbeb4dc9fab178
Thomas <Thomas@Tannenberg.online>
2023-11-08 13:12:24 +0100

    Erster Upload von IntelliJ

---
+++ src/TestPackage/Main.java
@@ -0,0 +1,9 @@
+package TestPackage;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //TEST
+        System.out.println("Das ist ein Tes zum pushen");
+    }
+}
